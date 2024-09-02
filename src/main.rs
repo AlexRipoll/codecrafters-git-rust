8adc8676cd4b8f7363ad5db1b4af9b5d58a62afd
Alex Ripoll <alexripollpiera@gmail.com>
2024-09-02 16:22:48 +0200

    cli args handling with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use core::panic;
+use clap::Parser;
+use clap::Subcommand;
 use flate2::bufread::ZlibDecoder;
 #[allow(unused_imports)]
 use std::env;
@@ -6,33 +7,49 @@ use std::env;
 use std::fs;
 use std::io::prelude::*;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Init,
+    CatFile {
+        #[arg(short = 'p')]
+        pretty_print: bool,
+
+        object_hash: String,
+    },
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args[1].as_str() {
-        "init" => {
+    let args = Args::parse();
+
+    match &args.command {
+        Commands::Init => {
             fs::create_dir(".git").unwrap();
             fs::create_dir(".git/objects").unwrap();
             fs::create_dir(".git/refs").unwrap();
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
         }
-        "cat-file" => match args[2].as_str() {
-            "-p" => {
-                let object_hash = &args[3];
-                let subdirectory = &object_hash[0..2];
-                let filename = &object_hash[2..];
+        Commands::CatFile {
+            pretty_print,
+            object_hash,
+        } => {
+            let subdirectory = &object_hash[0..2];
+            let filename = &object_hash[2..];
 
-                let object =
-                    fs::read(format!(".git/objects/{}/{}", subdirectory, filename)).unwrap();
+            let object = fs::read(format!(".git/objects/{}/{}", subdirectory, filename)).unwrap();
 
-                let mut d = ZlibDecoder::new(&object[..]);
-                let mut s = String::new();
-                d.read_to_string(&mut s).unwrap();
-                let pr = s.split_once("\0").unwrap();
-                print!("{}", pr.1);
-            }
-            _ => panic!("unknown flag"),
-        },
-        _ => println!("unknown command: {}", args[1]),
+            let mut d = ZlibDecoder::new(&object[..]);
+            let mut s = String::new();
+            d.read_to_string(&mut s).unwrap();
+            let pr = s.split_once("\0").unwrap();
+            print!("{}", pr.1);
+        }
     }
 }
